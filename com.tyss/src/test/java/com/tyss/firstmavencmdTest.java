d01de672b9d52d15a2bf9fcd1e21e9cb0910422e
Manu <Manu@Manu>
2022-08-24 11:34:10 +0530

    script1

---
+++ com.tyss/src/test/java/com/tyss/firstmavencmdTest.java
@@ -0,0 +1,25 @@
+package com.tyss;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class firstmavencmdTest {
+
+	
+	@Test
+	public void firstMethod()
+	{
+		System.out.println(" sucess");
+		Assert.fail();
+		
+	}
+	
+	@Test
+	public void secondMethod()
+	{
+		System.out.println(" second mehtod");
+	}
+	
+	
+
+}
